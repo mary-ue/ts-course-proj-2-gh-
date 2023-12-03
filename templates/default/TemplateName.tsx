842f6f680ae9b1de0cb503158fc71f721e8346c1
mary-ue <maryandreeva100@gmail.com>
2023-12-03 17:32:35 +0300

    add header

--- templates/default/TemplateName.tsx
+++ templates/default/TemplateName.tsx
@@ -1,9 +1,14 @@
+import React from 'react';
 import styles from './TemplateName.module.scss';
 
 interface TemplateNameProps { }
 
-export const TemplateName = ({ }: TemplateNameProps) => (
-  <div className={styles.TemplateName} data-testid="TemplateName">
-    TemplateName Component
-  </div>
-);
+export const TemplateName = ({ }: TemplateNameProps) => {
+
+
+  return (
+    <div className={styles.templateName}>
+      TemplateName Component
+    </div>
+  );
+};
